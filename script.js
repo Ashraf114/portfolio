d19ac37849166a5e82deaad20406eee1392f0227
vyendala <vyendala123@gmail.com>
2024-10-12 14:32:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example script for the contact form (placeholder)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
